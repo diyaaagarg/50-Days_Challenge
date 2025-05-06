098479079b411cdb65424801c5dc368a83e2d06a
diyaaa garg <gargdiya359@gmail.com>
2025-05-06 22:18:49 +0530

    both problems of day 01

--- Day01/Solution.java
+++ Day01/Solution.java
@@ -24,3 +24,16 @@ class Solution {
         return prev;
     }
 }
+// Missing Number
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor=0;
+        for(int i=1;i<=nums.length;i++){
+            xor=xor^i;
+        }
+        for(int i=0;i<nums.length;i++){
+            xor=xor^nums[i];
+        }
+        return xor;
+    }
+}
