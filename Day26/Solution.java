e7a80e208fbfd113094da8681f6a0dccda48130f
diyaaa garg <gargdiya359@gmail.com>
2025-06-08 18:33:36 +0530

    solved ques 2 only

--- Day26/Solution.java
+++ Day26/Solution.java
@@ -1,5 +1,31 @@
-package Day26;
-
-public class Solution {
-    
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int n=nums.length;
+        int low=0,high=n-1;
+        int[]ans={-1,-1};
+        while(low<=high){
+            int mid=low-(low-high)/2;
+            if(nums[mid]==target){
+                ans[0]=mid;
+                high=mid-1;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        low=0;high=n-1;
+        while(low<=high){
+            int mid=low-(low-high)/2;
+            if(nums[mid]==target){
+                ans[1]=mid;
+                low=mid+1;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
 }
