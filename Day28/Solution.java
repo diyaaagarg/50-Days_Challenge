afb17495081cf066a50c00e8a3a1e53ac6e6e74d
diyaaa garg <gargdiya359@gmail.com>
2025-06-10 22:45:13 +0530

    problem 1 not done(trees),problem 2 done

--- Day28/Solution.java
+++ Day28/Solution.java
@@ -1,3 +1,25 @@
-public class Solution {
-    
+//Problem-> 2470. Number of Subarrays With LCM Equal to K
+class Solution {
+    public int subarrayLCM(int[] nums, int k) {
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            int l=nums[i];
+            if(l==k)count++;
+            for(int j=i+1;j<nums.length;j++){
+                l=lcm(l,nums[j]);
+                if(l==k)count++;
+                if(l>k)break;
+            }
+        }
+        return count;
+    }
+    int lcm(int a,int b){
+        return a*b/gcd(a,b);
+    }
+    int gcd(int a,int b){
+        if(a%b==0){
+            return b;
+        }
+        return gcd(b,a%b);
+    }
 }
